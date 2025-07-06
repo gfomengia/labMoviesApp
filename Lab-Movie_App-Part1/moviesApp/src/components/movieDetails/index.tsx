416c6dd2c9097c761f3616694cb890584e584b59
Glen Fomengia <gfomengia@gmail.com>
2025-07-06 22:38:38 +0100

    Movie Details page components

---
+++ Lab-Movie_App-Part1/moviesApp/src/components/movieDetails/index.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Chip, Paper, Typography } from "@mui/material";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import MonetizationIcon from "@mui/icons-material/MonetizationOn";
+import StarRate from "@mui/icons-material/StarRate";
+import { MovieDetailsProps } from "../../types/interfaces";
+
+const styles = {
+  chipSet: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexWrap: "wrap",
+    listStyle: "none",
+    padding: 1.5,
+    margin: 0,
+  },
+  chipLabel: {
+    margin: 0.5,
+  },
+};
+
+const MovieDetails: React.FC<MovieDetailsProps> = (movie) => {
+  return (
+    <>
+      <Typography variant="h5" component="h5">
+        Overview
+      </Typography>
+
+      <Typography variant="h5" component="p">
+        {movie.overview}
+      </Typography>
+
+      <Paper component="ul" sx={styles.chipSet}>
+        <li>
+          <Chip label="Genres" sx={styles.chipLabel} color="primary" />
+        </li>
+        {movie.genres.map((g) => (
+          <li key={g.name}>
+            <Chip label={g.name} />
+          </li>
+        ))}
+      </Paper>
+      <Paper component="ul" sx={styles.chipSet}>
+        <Chip icon={<AccessTimeIcon />} label={`${movie.runtime} min.`} />
+        <Chip
+          icon={<MonetizationIcon />}
+          label={`${movie.revenue.toLocaleString()}`}
+        />
+        <Chip
+          icon={<StarRate />}
+          label={`${movie.vote_average} (${movie.vote_count})`}
+        />
+        <Chip label={`Released: ${movie.release_date}`} />
+      </Paper>
+    </>
+  );
+};
+
+export default MovieDetails;
---
+++ Lab-Movie_App-Part1/moviesApp/src/stories/movieDetails.stories.ts
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import MovieDetails from "../components/movieDetails";
+import SampleMovie from "./sampleData";
+
+
+const meta = {
+    title: "Movie Details/MovieDetails",
+    component: MovieDetails,
+} satisfies Meta<typeof MovieDetails>;
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+export const Basic: Story = {
+    args: SampleMovie
+};
+Basic.storyName = "Default"
