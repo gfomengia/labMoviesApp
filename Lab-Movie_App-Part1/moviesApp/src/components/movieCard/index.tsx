2c668daf8691965f77bd50a0828820a16e2ca680
Glen Fomengia <gfomengia@gmail.com>
2025-07-06 11:52:42 +0100

    add home page components rename stories file extension to correct format

---
+++ Lab-Movie_App-Part1/moviesApp/src/components/movieCard/index.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { BaseMovieProps } from "../../types/interfaces";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Grid,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import CalendarIcon from "@mui/icons-material/CalendarTodayTwoTone";
+import StarRateIcon from "@mui/icons-material/StarRate";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import img from "../../images/film-poster-placeholder.png";
+
+const styles = {
+  card: { maxWidth: 345 },
+  media: { height: 500 },
+  avatar: {
+    backgroundColor: "rgb(255, 0, 0)",
+  },
+};
+
+const MovieCard: React.FC<BaseMovieProps> = (movie) => {
+  return (
+    <Card sx={styles.card}>
+      <CardHeader title={movie.title} />
+      <CardMedia
+        sx={styles.media}
+        image={
+          movie.poster_path
+            ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+            : img
+        }
+      />
+      <CardContent>
+        <Grid container>
+          <Grid item xs={6}>
+            <Typography variant="h6" component="p">
+              <CalendarIcon fontSize="small" />
+              {movie.release_date}
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="h6" component="p">
+              <StarRateIcon fontSize="small" /> {movie.vote_average}{" "}
+            </Typography>
+          </Grid>
+        </Grid>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favoutites">
+          <FavoriteIcon color="primary" fontSize="large" />
+        </IconButton>
+        <Button variant="outlined" size="medium" color="primary">
+          More Info ...
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default MovieCard;
